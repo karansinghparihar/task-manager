4b5d8ef9dc13c2ae1ffb319aac085cfdf6afeea1
karan.singh.parihar <karan.singh.parihar@accenture.com>
2021-12-16 10:04:06 +0530

    first message.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Task Manager (In development stage....)</h1>
+       <h3>Thank you for your patience!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
